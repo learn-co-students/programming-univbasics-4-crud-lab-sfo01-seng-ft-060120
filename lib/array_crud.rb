0ead5b50073b8d2c3b2de598b3ea8b96ee673608
Iona <ionabrabender@gmail.com>
2020-04-24 23:03:55 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,39 @@
 def create_an_empty_array
-  
+  array = []
 end
 
 def create_an_array
-  
+  array = ["apple", "banana", "canteloupe", "dragonfruit"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  p array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  p array [0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  p array[(last_element = (array.length - 1))]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  p array[index_number] = element
 end
